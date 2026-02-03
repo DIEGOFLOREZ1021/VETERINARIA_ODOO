7b4ec4dd7b679ef569cf82da5d71e9db66ccae31
Jorge Cristancho <damjorgecristancho@gmail.com>
2026-02-03 12:19:44 +0100

    Segunda_Entrega_Gestion_Equipo

--- addons/gestion_equipo/controllers/main.py
+++ addons/gestion_equipo/controllers/main.py
@@ -2,14 +2,14 @@ from odoo import http
 from odoo.http import request
 
 class GestionEquipo(http.Controller):
-    @http.route('/gestion_equipo', auth='public', website=True)
+    @http.route('/equipo', auth='public', website=True)
     def index(self, **kwargs):
-        equipos = request.env['gestion.persona'].sudo().search([])
+        equipo = request.env['gestion.persona'].sudo().search([])
         return request.render('gestion_equipo.web_equipo_list', {
-            'equipos': equipos
+            'equipo': equipo
         })
     @http.route('/equipo/<model("gestion.persona"):persona>', auth='public', website=True)
     def equipo_detail(self, persona, **kw):
-        return http.request.render('gestion_equipo.web_equipo_detail', {
+        return http.request.render('gestion_equipo.web_persona_detail', {
             'persona': persona
         })
---
+++ addons/gestion_equipo/models/persona.py
@@ -0,0 +1,13 @@
+from odoo import models, fields
+
+class GestionPersona(models.Model):
+    _name = 'gestion.persona'  
+    _description = 'Persona del Equipo'
+
+    img = fields.Binary(string='Imagen')
+    name = fields.Char(string='Nombre', required=True)
+    lastname = fields.Char(string='Apellido', required=True)
+    age = fields.Integer(string='Edad')
+    grade = fields.Char(string='Grado o Especialidad')
+    active = fields.Boolean(string='Activo')
+    description = fields.Text(string='Descripción personal')
