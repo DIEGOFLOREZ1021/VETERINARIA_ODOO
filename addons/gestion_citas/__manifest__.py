d742ad735f8f07edb60c4135b4785e861fff246d
Aitor215 <aitorrov@gmail.com>
2026-02-18 17:17:09 +0100

    subiendo modulo de citas

--- addons/gestion_citas/__manifest__.py
+++ addons/gestion_citas/__manifest__.py
@@ -1,34 +1,29 @@
 # -*- coding: utf-8 -*-
 {
     'name': "gestion_citas",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
+    'summary': "Aplicacion para gestionar citas veterinarias",
     'description': """
-Long description of module's purpose
+Esta aplicacion puedes gestionar a que hora, con quien y un poco de informacion del animal para hacer una cita con el cliente o el cliente contigo
     """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Aitor",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
+    'category': 'Services',
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/gestion_cita_views.xml',
+        'views/gestion_cita_search.xml',
+        'views/gestion_cita_menu.xml',
     ],
+    'installable': True,
+    'application': True,
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+
 }
 
